0ba11c8629754d000e5160bbf98e12e6d2778532
janast-deree <a.anastasopoulou@acg.edu>
2022-12-09 15:29:46 +0200

    deployment - production settings

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,15 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
+
+from .sitemaps import StaticSitemap
 from .views import *
+
+sitemaps = {
+    'static': StaticSitemap
+}
 
 urlpatterns = [
     path('', index, name='home'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('filter_pieces', filter_pieces, name='filter_pieces'),
 ]
--- showcase/settings.py
+++ showcase/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'crispy_forms',
     'crispy_bootstrap5'
 ]
